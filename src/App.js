93bdad7bec18bae16dcb660aa0d9fa4a72a3ca90
Vaibhav Balooni <phoenixshan16@gmail.com>
2024-06-03 02:08:03 +0530

    Frontend Update

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import axios from 'axios';
 
 function App() {
   const [keyword, setKeyword] = useState('');
+  const [selectedItem, setSelectedItem] = useState('');
 
   const handleKeywordChange = (event) => {
     setKeyword(event.target.value)
   }
 
+  const handleCheckboxChange = (item) => {
+    setSelectedItem(item);
+  };
+
+  const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post('/data',{key: keyword})
+    axios.post('/data', { key: keyword , check: selectedItem})
   };
 
   return (
@@ -23,6 +30,20 @@ function App() {
         <button type="submit">Submit</button>
       </form>
 
+      <div>
+      <h2>Checklist</h2>
+      {/* Render checkboxes */}
+      {items.map((item, index) => (
+        <div key={index}>
+          <input
+            type="checkbox"
+            checked={selectedItem === item} // Set checked state based on selectedItem
+            onChange={() => handleCheckboxChange(item)} // Handle checkbox change
+          />
+          <label>{item}</label>
+        </div>
+      ))}
+    </div>
     </div>
   );
 }
